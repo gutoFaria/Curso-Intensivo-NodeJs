637ec24d588db6a55b319aefb717e51623e692b2
Éster Augusto <61546216+gutoFaria@users.noreply.github.com>
2023-01-10 11:01:08 -0300

    curso

---
+++ person.js
@@ -0,0 +1,21 @@
+
+(function(exports,require,module,__filename,__dirname){
+
+})
+
+console.log(__dirname,__dirname);
+
+class Person{
+
+    constructor(name,age){
+        this.name = name;
+        this.age = age
+    }
+    
+
+    greeting(){
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+    }
+}
+
+module.exports = Person
